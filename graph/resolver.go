51819c8586cd897646b5b242e9442665c6325889
Mateus Sousa <sousa.programador@gmail.com>
2023-05-19 16:50:24 -0300

    generate resolver

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,7 +1,10 @@
 package graph
 
+import "github.com/sousaprogramador/graphQL-GoLang/internal/database"
 // This file will not be regenerated automatically.
 //
 // It serves as dependency injection for your app, add any dependencies you require here.
 
-type Resolver struct{}
+type Resolver struct{
+	CategoryDB *database.Category
+}
